6ccbaa62bde4c44fea5222bb7f7b3257114897d2
vilesport <89498002+vilesport@users.noreply.github.com>
2024-10-28 22:04:00 +0700

    Update BT1.cpp

--- Lab2/BT1/BT1.cpp
+++ Lab2/BT1/BT1.cpp
@@ -9,26 +9,33 @@ int main()
 
 	cout << "a: ";
 	a.Xuat();
+	cout << "\n";
 	cout << "b: ";
 	b.Xuat();
+	cout << "\n";
 	cout << "c: ";
 	c.Xuat();
+	cout << "\n";
 
 	a.SetTungDo(b.GetTungDo());
 	a.SetHoanhDo(c.GetHoanhDo());
 
 	cout << "a: ";
 	a.Xuat();
+	cout << "\n";
 	cout << "b: ";
 	b.Xuat();
+	cout << "\n";
 	cout << "c: ";
 	c.Xuat();
+	cout << "\n";
 
 	cout << "Nhap vector tinh tien A(u, v): ";
 	cin >> u >> v;
 	a.TinhTien(u, v);
 
 	cout << "Diem sau khi tinh tien: ";
-	a.Xuat();  // Xuat toa do cua diem a sau khi tinh tien
+	a.Xuat();
+	cout << "\n";
 	return 0;
 }
