ac75f477d3841b2f0170dbd08a4792184f0d1235
vilesport <89498002+vilesport@users.noreply.github.com>
2024-10-28 21:25:01 +0700

    Update Tamgiac.cpp

--- Lab2/BT2/Tamgiac.cpp
+++ Lab2/BT2/Tamgiac.cpp
@@ -54,6 +54,7 @@ void TamGiac::TinhTien(float u, float v)
  * @brief Phong to tam giac theo tam O voi ti le k.
  * @param O Diem tam phong to
  * @param k Ti le phong to
+ * Phong to tam giac bang cach phong to 3 dinh cua tam giac.
  */
 void TamGiac::PhongTo(Diem O, float k)
 {
@@ -73,6 +74,7 @@ void TamGiac::PhongTo(Diem O, float k)
  * @brief Thu nho tam giac theo tam O voi ti le k.
  * @param O Diem tam thu nho
  * @param k Ti le thu nho
+ * Thu nho tam giac bang cach phong to tam giac voi ti le 1/k
  */
 void TamGiac::ThuNho(Diem O, float k)
 {
@@ -84,6 +86,7 @@ void TamGiac::ThuNho(Diem O, float k)
  * @brief Quay tam giac quanh diem O voi goc quay theta.
  * @param theta Goc quay tinh bang do
  * @param O Diem tam quay
+ * Quay tam giac bang cach quay 3 dinh cua tam giac.
  */
 void TamGiac::Quay(float theta, Diem O)
 {
