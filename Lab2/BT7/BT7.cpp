2a86df2aa17c24182a7e6d0a0a40d21255c97eac
vilesport <89498002+vilesport@users.noreply.github.com>
2024-10-28 21:10:56 +0700

    Update BT7.cpp

--- Lab2/BT7/BT7.cpp
+++ Lab2/BT7/BT7.cpp
@@ -2,9 +2,6 @@
 
 Poly A;
 
-/**
- * @brief Diem bat dau cua chuong trinh. Thuc hien cac thao tac nhap, sap xep cac dinh cua da giac va tinh dien tich.
- */
 int main() {
     A.Nhap();
     A.Sapxep();
