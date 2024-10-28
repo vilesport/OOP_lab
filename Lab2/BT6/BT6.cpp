f1ebc173609d86ead1b8272c47a8724a972c5c9c
vilesport <89498002+vilesport@users.noreply.github.com>
2024-10-28 21:17:22 +0700

    Update BT6.cpp

--- Lab2/BT6/BT6.cpp
+++ Lab2/BT6/BT6.cpp
@@ -3,12 +3,6 @@
 TamGiac a;
 float degree, v;
 
-/**
- * @brief Diem bat dau cua chuong trinh. Thuc hien nhap, tinh tien va xuat thong tin tam giac.
- * 
- * Chuong trinh cho phep nguoi dung nhap thong tin cua tam giac, sau do thuc hien tinh tien tam giac 
- * theo goc quay va khoang cach da nhap, va cuoi cung xuat thong tin tam giac sau khi tinh tien.
- */
 int main()
 {
     a.Nhap();
